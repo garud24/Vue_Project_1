7bf9e9b8bfbafde662d4e364474573d218b32a57
Himanshu Garud <himanshugarud@Himanshus-MacBook-Pro.local>
2024-06-13 12:38:00 -0400

    v-if and v-else

---
+++ vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+function saymyName(num){
+  return num + 'Shivani'
+}
+</script>
+
+<template>
+  <h1 class="red">Hello World</h1>
+
+  <div>  </div>
+  <!-- <div v-if="false">My name is {{ saymyName(10) }} </div> -->
+   <h1 v-if="false"> My name is {{ saymyName(10) }}</h1>
+   <h1 v-else="true">My name is {{ 'Himanshu'.toLocaleUpperCase() }}</h1>
+</template>
+
+<style>
+.red{
+  color:red
+}
+</style>
+
